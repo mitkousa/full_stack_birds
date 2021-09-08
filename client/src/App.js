8555ac472830946093039a35855f5c6883662223
mitkousa <uk.dimitar@gmail.com>
2021-09-08 10:30:37 +0100

    extensions done

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import SightingsContainer from './containers/SightingsContainer';
+
+
+function App() {
+
+ return <SightingsContainer/>
+}
+
+export default App;
---
+++ client/src/SightingService.js
@@ -0,0 +1,31 @@
+const baseURL = 'http://localhost:5000/api/sightings/'
+
+export const getSightings = () => {
+    return fetch(baseURL)
+        .then(res => res.json())
+}
+
+export const postSighting = (payload) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+}
+
+// export const updateSighting = (payload) => {
+//     return fetch(baseURL + id, {
+//         method: 'PUT',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json' }
+//     })
+//     .then(res => res.json())
+// }
+
+export const deleteSighting = (id) => {
+    return fetch(baseURL + id, {
+        method: 'DELETE'
+    })
+}
+
